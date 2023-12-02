0973c99099a871fe059621dcadbfa6357dee64cc
Diana <dianamingazova162@gmail.com>
2023-12-03 00:18:04 +0500

    Начальный коммит

---
+++ EmployeeBook9/src/main/java/SkyPro/EmployeeBook9/EmployeeBook9/EmployeeBook9Application.java
@@ -0,0 +1,13 @@
+package SkyPro.EmployeeBook9.EmployeeBook9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBook9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBook9Application.class, args);
+	}
+
+}
---
+++ EmployeeBook9/src/test/java/SkyPro/EmployeeBook9/EmployeeBook9/EmployeeBook9ApplicationTests.java
@@ -0,0 +1,13 @@
+package SkyPro.EmployeeBook9.EmployeeBook9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBook9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
